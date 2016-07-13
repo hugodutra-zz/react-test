ece5532844833daaf5d237a7144d9da8c7daddc7
Hugo Dutra <hugow.dutra@gmail.com>
2016-07-13 16:55:05 -0300

    gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,14 @@
 "use strict";
 
 const gulp = require('gulp');
-const browserify = require('browserify');
-const connect = require('gulp-connect');
 const babel = require('gulp-babel');
+const webpack = require('webpack-stream');
 
-gulp.task('build-server', (done) => {
-    glob('./src/*.js', (err, files) => {
-        if (err) done(err)
-
-        let tasks = files.map((entry) => {
-            return gulp.src(entry)
-                .pipe(babel({ presets: ['es2015'] }))
-                .pipe(gulp.dest('./build'));
-        });
-    });
-});
-
-gulp.task('build', ['build-client', 'build-server'])
-
-gulp.task('watch-client', () => {
-    gulp.watch('./app/**/*', ['build-client'], (e) => {
-        console.log('Client file ' + e.path + ' was ' + e.type + ', rebuilding...')
-    })
-})
-
-gulp.task('watch-server', () => {
-    gulp.watch('./src/**/*', ['build-server'], (e) => {
-        console.log('Server file ' + e.path + ' was ' + e.type + ', rebuilding...')
-    })
-})
-
-gulp.task('watch', ['watch-client', 'watch-server'])
+gulp.task('babel', () =>
+    gulp.src('src/app.js')
+        .pipe(babel({
+            presets: ['es2015', 'react']
+        }))
+        .pipe(webpack( require('./webpack.config.js') ))
+        .pipe(gulp.dest('dist'))
+);
